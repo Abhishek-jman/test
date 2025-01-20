ea0d280914849fe4aad072441746ca815d29edba
jmd301-abhishek <abhishekkumar@jmangroup.com>
2025-01-20 15:49:14 +0530

    Delete PMC-Platform-Build-main directory

--- PMC-Platform-Build-main/PMC_DWH/models/01_stg/teamwork/users.sql
+++
@@ -1,9 +0,0 @@
-{{config(
-    tags = ["staging" , "teamwork_staging"]
-) }}
-
-With source as (
-    select title , count(title) from {{ source('staging','users') }} group by title
-)
-
-select * from source
